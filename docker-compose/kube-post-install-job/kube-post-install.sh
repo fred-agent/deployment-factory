a917780f37e4b6448e32bb8bdee004b6663f0b6a
Fabien LE SOLLIEC <fabien.le-solliec@thalesgroup.com>
2025-06-12 16:05:41 +0200

    Improve kubernetes templating - add kubernetes-dashboard in progress

--- docker-compose/kube-post-install-job/kube-post-install.sh
+++ docker-compose/kube-post-install-job/kube-post-install.sh
@@ -21,7 +21,7 @@ apk add --no-cache kubectl helm
 # Patch kubeconfig
 if ! kubectl config get-contexts | grep -q k8s-cluster-local
 then
-    kubectl config set-cluster default --server=https://fred-kube:6443
+    kubectl config set-cluster default --server=https://app-kube:6443
     kubectl config rename-context default k8s-cluster-local
 fi
 
@@ -30,3 +30,11 @@ helm install kafka oci://registry-1.docker.io/bitnamicharts/kafka --namespace me
 helm install nginx oci://registry-1.docker.io/bitnamicharts/nginx --namespace web --create-namespace
 helm install redis oci://registry-1.docker.io/bitnamicharts/redis --namespace cache --create-namespace
 helm install postgresql oci://registry-1.docker.io/bitnamicharts/postgresql --namespace database --create-namespace
+helm install dashboard oci://registry-1.docker.io/bitnamicharts/kubernetes-dashboard --namespace dashboard --create-namespace
+
+# # Install kubernetes dashboard
+# helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+# helm repo update
+# helm install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --namespace dashboard --create-namespace \
+#     --set app.ingress.enabled=true \
+#     --set app.ingress.hosts[0]=${DOCKER_COMPOSE_HOST_FQDN:-localhost}
